3a47c952338ce74362369b5d4709a8df0161f9e8
Victor Lyapunov <viclyapunov@users.noreply.github.com>
2020-01-05 17:29:23 +0300

    432

--- app/Http/Controllers/kizController.php
+++ app/Http/Controllers/kizController.php
@@ -2,17 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Goutte\Client;
-
-class kizController extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
-
 
 
 $client = new Client();
